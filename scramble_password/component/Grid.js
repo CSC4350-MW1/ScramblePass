f7aa4d9a1538888e026b2cc0abfbccde9ed3e9b5
Daniel Rochefort <danielrochefort99@gmail.com>
2022-11-28 11:55:34 -0500

    added a grid element

---
+++ scramble_password/component/Grid.js
@@ -0,0 +1,14 @@
+export function Grid({ children, columns }) {
+  return (
+    <div
+      style={{
+        display: 'grid',
+        gridTemplateColumns: `repeat(${columns}, 1fr)`,
+        gridGap: 10,
+        padding: 10,
+      }}
+    >
+      {children}
+    </div>
+  );
+}
--- scramble_password/firebaseAdmin.js
+++ scramble_password/firebaseAdmin.js
@@ -1,14 +1,19 @@
 const admin = require("firebase-admin");
+const { getStorage } = require('firebase-admin/storage');
 const serviceAccount = require("./secrets.json");
 
 export const verifyIdToken = (token) => {
     if (!admin.apps.length) {
         admin.initializeApp({
-            credential: admin.credential.cert(serviceAccount)
+            credential: admin.credential.cert(serviceAccount),
+            storageBucket: 'scrambler-pass.appspot.com'
         });
+
     }
 
     return admin.auth().verifyIdToken(token).catch((error) => {
         throw error;
     })
 };
+// 
+export const bucket = getStorage().bucket();
